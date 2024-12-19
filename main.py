275e67162ab49fe9e53d2dcccedca58b98b925f1
Clayton Yochum <claytonjy@gmail.com>
2024-12-18 20:32:28 -0700

    Replace world with dynamic name.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, world!"}
+@app.get("/{name}")
+def read_root(name):
+    return {"message": f"Hello, {name}!"}
